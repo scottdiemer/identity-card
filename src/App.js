112b83b6a2e67ce42fdaf14a1bdb7942b6940d6e
scottdiemer <sdiemer@cfl.rr.com>
2020-08-21 14:35:33 -0400

    started adding css in component with styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import IdentityCard from "./identityCard";
 import "./App.css";
+import avatarImage from "./images/avatar.jpg";
 
 function App() {
   return (
     <div className="App">
-      <IdentityCard name="Scott Diemer" image="" />
+      <IdentityCard name="Scott Diemer" image={avatarImage} />
     </div>
   );
 }
--- src/identityCard.js
+++ src/identityCard.js
@@ -1,8 +1,9 @@
 import React from "react";
+import styled from "styled-components";
 
 function IdentityCard(props) {
   return (
-    <div id="wrapper">
+    <StyledWrapper id="wrapper">
       <section id="main">
         <span className="avatar">
           <img src={props.image} alt={props.name} />
@@ -10,8 +11,43 @@ function IdentityCard(props) {
         <h1>{props.name}</h1>
         <p>Software Engineer</p>
       </section>
-    </div>
+    </StyledWrapper>
   );
 }
+
+const StyledWrapper = styled.div`
+  text-align: center;
+  #main {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    transition: 0.3s;
+    border-radius: 5px;
+    padding: 5em;
+  }
+
+  .avatar {
+    position: relative;
+    display: block;
+    margin-bottom: 1.5em;
+  }
+  img {
+    display: block;
+    margin: 0 auto;
+    border-radius: 100%;
+    box-shadow: 0 0 0 1.5em #ffffff;
+    height: 100px;
+    width: 100px;
+  }
+  .avatar:before {
+    content: "";
+    display: block;
+    position: absolute;
+    top: 50%;
+    left: -3em;
+    width: calc(100% + 6em);
+    height: 1px;
+    z-index: -1;
+    background: #c8cccf;
+  }
+`;
 
 export default IdentityCard;
